b540b84e5441fd966770aa77a6558ddfb8e8c745
Matheus Oliveira <mateusribeirooliveira@hotmail.com>
2022-05-06 17:57:15 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import { Widget } from './components/Widget'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Widget/>
+  )
+}
+
+export default App
